fbf5133de6e8c892f05635077bca9236055f6f18
Mikey Leibbrandt <mike.leibbrandt@gmail.com>
2023-12-15 13:34:03 +0200

    SQL Updates: REPLACE INTO

---
+++ code/sql/insert_into.sql
@@ -0,0 +1,2 @@
+INSERT INTO table_name (column1, column2, column3)
+VALUES (column1_value, column2_value, column3_value);
---
+++ code/sql/replace_into.sql
@@ -0,0 +1,6 @@
+REPLACE [INTO] table_name(column_list)
+VALUES(value_list);
+
+REPLACE INTO table
+SET column1 = value1,
+    column2 = value2;
