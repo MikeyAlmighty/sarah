1894e7d8148ec5a8a03e2253b56986cc7c4e277f
Mikey Leibbrandt <mike.leibbrandt@gmail.com>
2023-12-24 19:53:10 +0200

    OCP WIP Chapter 2 Type Conversions

--- code/java/type_conversions.java
+++ code/java/type_conversions.java
@@ -1,3 +1,8 @@
 Object obj = "Upcast Me"; // Widening - Implicit
 
 String casted = (String) obj; // Narrowing - requires cast
+
+Integer iRef = 10;  // (1) Implicit boxing: Integer <----- int
+Double dRef = Double.valueOf(3.14); // (2) Explicit boxing: Double <----- double
+int i = iRef; // (3) Implicit unboxing: int <----- Integer
+double d = dRef.doubleValue(); // (4) Explicit unboxing: double <----- Double
