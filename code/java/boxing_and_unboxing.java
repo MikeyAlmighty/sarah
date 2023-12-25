8a4d6b7e062d5db50dc6648a0bc87cd6b4d59999
Mikey Leibbrandt <mike.leibbrandt@gmail.com>
2023-12-25 08:43:44 +0200

    OCP Chapter 2 - Boxing and unboxing

---
+++ code/java/boxing_and_unboxing.java
@@ -0,0 +1,5 @@
+Integer iRef = 10;  // (1) Implicit boxing: Integer <----- int
+Double dRef = Double.valueOf(3.14); // (2) Explicit boxing: Double <----- double
+
+int i = iRef; // (3) Implicit unboxing: int <----- Integer
+double d = dRef.doubleValue(); // (4) Explicit unboxing: double <----- Double
