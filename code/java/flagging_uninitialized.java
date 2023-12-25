adf9557883b3b884b1f3b498aca0c20000df8ea8
Mikey Leibbrandt <mike.leibbrandt@gmail.com>
2023-12-25 19:06:14 +0200

    OCP Chapter 3 : WIP

---
+++ code/java/flagging_uninitialized.java
@@ -0,0 +1,17 @@
+public class TooSmartClass {
+  public static void main(String[] args) {
+    double weight = 10.0, thePrice;                    // (1) Local variables
+
+    if (weight <  10.0) thePrice = 20.50;
+    if (weight >  50.0) thePrice = 399.00;
+    if (weight >= 10.0) thePrice = weight * 10.0;      // (2) Always executed
+    System.out.println("The price is: " + thePrice);   // (3) Compile-time error!
+  }
+}
+
+public class VerySmartClass {
+    public static void main(String[] args) {
+        String importantMessage; // Local reference variable
+        System.out.println("The message length is: " + importantMessage.length());// Compile-time error!
+    }
+}
