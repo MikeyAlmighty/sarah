3317a3158a91535181415136b19a36bc35d1d8d1
Mikey Leibbrandt <mike.leibbrandt@gmail.com>
2023-12-30 05:40:37 +0200

    OCP Chapter 1 - JAR and javac additions

---
+++ code/ocp/1/new_package.java
@@ -0,0 +1,14 @@
+package packagea;
+
+public class ClassA {}
+
+package packageb;
+
+import packagea.ClassA;
+
+public class ClassB {
+    public static void main(String[] args) {
+        ClassA a;
+        System.out.println("Got it");
+    }
+}
