7a653e483595b0f20173326cb20115ba9a37e771
Mikey Leibbrandt <mike.leibbrandt@gmail.com>
2024-01-01 10:01:38 +0200

    OCP Chapter 2 - Added ternary unperformed side effects section

--- code/ocp/2/ternary_operators.java
+++ code/ocp/2/ternary_operators.java
@@ -6,3 +6,13 @@ System.out.println(food); // 4
 int stripes = 7;
 System.out.print((stripes > 5) ? 21 : "Zebra");
 int animal = (stripes < 9) ? 3 : "Horse"; // DOES NOT COMPILE
+
+int sheep = 1;
+int zzz = 1;
+int sleep = zzz < 10 ? sheep++ : zzz++;
+System.out.print(sheep + "," + zzz); // 2,1
+
+int sheep = 1;
+int zzz = 1;
+int sleep = sheep >= 10 ? sheep++ : zzz++;
+System.out.print(sheep + "," + zzz); // 1,2
