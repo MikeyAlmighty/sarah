fbc4b06d8e796728d1fdff26856ccc2efb32347b
Mikey Leibbrandt <mike.leibbrandt@gmail.com>
2024-01-02 17:10:12 +0200

    OCP Chapter 3. - For Each Loops additions

--- code/ocp/3/for_each_loop.java
+++ code/ocp/3/for_each_loop.java
@@ -9,3 +9,11 @@ String[] fruits = { "Apple", "Mango", "Orange" };
 for (String fruit: fruits) {
   System.out.println(fruit);
 }
+
+String birds = "Jay";
+for(String bird : birds) // DOES NOT COMPILE
+    System.out.print(bird + " ");
+
+String[] sloths = new String[3];
+for(int sloth : sloths) // DOES NOT COMPILE
+    System.out.print(sloth + " ");
