70b63f5df0f01c908422e62cf450c2a33ef3b9d0
Mikey Leibbrandt <mike.leibbrandt@gmail.com>
2024-01-02 10:28:13 +0200

    OCP Chapter 3 - Finished Switch Expressions

--- code/ocp/3/switch_expressions.java
+++ code/ocp/3/switch_expressions.java
@@ -12,11 +12,41 @@ public void printDayOfWeek(int day) {
     System.out.print(result);
 }
 
-public void printSeason(int month) {
-    switch(month) {
-        case 1, 2, 3 -> System.out.print("Winter");
-        case 4, 5, 6 ­> System.out.print("Spring");
-        case 7, 8, 9 -> System.out.print("Summer");
-        case 10, 11, 12 -> System.out.print("Fall");
+int measurement = 10;
+int size = switch(measurement) {
+    case 5 -> 1;
+    case 10 -> (short)2;
+    default -> 5;
+    case 20 -> "3"; // DOES NOT COMPILE
+    case 40 -> 4L; // DOES NOT COMPILE
+    case 50 -> null; // DOES NOT COMPILE
+};
+
+int fish = 5;
+int length = 12;
+var name = switch(fish) {
+    case 1 -> "Goldfish";
+    case 2 -> { yield "Trout"; }
+    case 3 -> {
+        if(length > 10)
+            yield "Blobfish";
+        else
+            yield "Green";
     }
+    default -> "Swordfish";
+};
+
+public static String printSeason(int month) throws IllegalStateException {
+    return switch(month) {
+        case 1, 2, 3 -> "Winter";
+        case 4, 5, 6 -> "Spring";
+        case 7, 8, 9 -> "Summer";
+        case 10, 11, 12 -> "Fall";
+        default -> throw new IllegalStateException("Unexpected value: " + month);
+    };
 }
+
+var name = switch(fish) {
+    case 1 -> "Goldfish" // DOES NOT COMPILE (missing semicolon)
+    case 2 -> { yield "Trout"; }; // DOES NOT COMPILE (extra semicolon)
+} // DOES NOT COMPILE (missing semicolon)
