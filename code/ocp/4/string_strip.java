3c5615e6201a66f14590d1558e936e5c2ac0bd4f
Mikey Leibbrandt <mike.leibbrandt@gmail.com>
2024-01-04 14:47:40 +0200

    OCP Chapter 4 - Strip and Trim

---
+++ code/ocp/4/string_strip.java
@@ -0,0 +1,11 @@
+public String strip()
+public String stripLeading()
+public String stripTrailing()
+
+System.out.println("abc".strip()); // abc
+System.out.println("\t    a b c\n".strip()); // a b c
+
+String text = " abc\t ";
+System.out.println(text.strip().length()); // 3
+System.out.println(text.stripLeading().length()); // 5
+System.out.println(text.stripTrailing().length());// 4
---
+++ code/ocp/4/string_trim.java
@@ -0,0 +1,5 @@
+public String trim()
+
+String text = " abc\t ";
+
+System.out.println(text.trim().length()); // 3
