ffe3475bb9da50858301274cb5b0308f65ec179d
Mikey Leibbrandt <mike.leibbrandt@gmail.com>
2024-01-07 08:20:32 +0200

    OCP Chapter 4. - Arrays usage

---
+++ code/ocp/4/array_usage.java
@@ -0,0 +1,11 @@
+String[] mammals = {"monkey", "chimp", "donkey"};
+System.out.println(mammals.length); // 3
+System.out.println(mammals[0]); // monkey
+System.out.println(mammals[1]); // chimp
+System.out.println(mammals[2]); // donkey
+
+String[] mammals = {"monkey", "chimp", "donkey"};
+System.out.println(mammals.length()); // DOES NOT COMPILE
+
+var birds = new String[6];
+System.out.println(birds.length); // 6
