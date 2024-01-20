90cfb6d2204b0cad92845763ad2d57f91eeafe06
Mikey Leibbrandt <mike.leibbrandt@gmail.com>
2024-01-20 14:18:18 +0200

    OCP Chapter 6. - Finished Inheriting Members

---
+++ code/ocp/6/final_methods.java
@@ -0,0 +1,13 @@
+public class Bird {
+    public final boolean hasFeathers() {
+        return true;
+    }
+    public final static void flyAway() {}
+}
+
+public class Penguin extends Bird {
+    public final boolean hasFeathers() { // DOES NOT COMPILE
+        return false;
+    }
+    public final static void flyAway() {} // DOES NOT COMPILE
+}
